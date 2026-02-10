ae0a4e11d0141949ee1e90563f268a139c322e1e
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-02-10 19:51:18 +0530

    make the scalatin of userController

---
+++ Server/controllers/user.controller.js
@@ -0,0 +1,19 @@
+const register = (req , res)=>{
+
+}
+const login = (req , res)=>{
+
+}
+const logout = (req , res)=>{
+
+}
+const getprofile = (req , res)=>{
+
+}
+
+export {
+    register,
+    login,
+    logout,
+    getprofile
+}
--- Server/routes/user.route.js
+++ Server/routes/user.route.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
+import {register , login , logout , getprofile} from '../controllers/user.controller.js'
 
 const router = Router();
 
 router.post("/register" , register);
 router.post("/login" , login);
 router.get("/logout" , logout);
-router.post("/me" , profile);
+router.post("/me" , getprofile);
 
 export default router;
