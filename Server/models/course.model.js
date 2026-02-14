6f19e469bc207acbb94e98c5799beab3e5bc3555
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-02-14 21:58:22 +0530

    course Schema part done

--- Server/models/course.model.js
+++ Server/models/course.model.js
@@ -1,35 +1,65 @@
-import mongoose, { Schema , model } from "mongoose";
+import { Schema, model } from "mongoose";
 
-const courseSchema = new mongoose.Schema({
-    title : {
-        type : String,
-        maxLength : [40 , 'Title Should be less then 40 character'],
-        required : true,
-        trim : true,
+const courseSchema = new Schema(
+  {
+    title: {
+      type: String,
+      maxLength: [40, "Title Should be less then 40 character"],
+      minLength: [8, "Title must be at least 8  character"],
+      required: [true, "title is Required"],
+      trim: true,
     },
-    Description : {
-        type : String,
-        minLength : [4 , 'Title Should be at least 4 character'],
-        maxLength : [400 , 'Title Should be less then 400 character'],
-        required : true,
-        trim : true,
+    Description: {
+      type: String,
+      minLength: [4, "Title Should be at least 4 character"],
+      maxLength: [400, "Title Should be less then 400 character"],
+      required: [true, "Description is Required"],
+      trim: true,
     },
-    lecture :{
+    category: {
+      type: String,
+      minLength: [4, "Title must be at least 4 character"],
+      maxLength: [400, "Title Should be less then 400 character"],
+      required: [true, "category is Required"],
+      trim: true,
+    },
+    createdBy: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lectures: [
+      {
         type: String,
-    
-    },
-    banner : {  
-        public_id :{
-            type : String
+        doescription: String,
+        lecture: {
+          public_id: {
+            type: String,
+          },
+          secure_url: {
+            type: String,
+          },
         },
-        secure_url :{
-            type : String
-        }    },
+      },
+    ],
+    noOfLecture: {
+      type: Number,
+      default : 0;
+    },
+    thumbnail: {
+      public_id: {
+        type: String,
+      },
+      secure_url: {
+        type: String,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-} , {
-    timestamps : true,
-})
-
-const COURSE = new model("course" , courseSchema);
+const COURSE = new model("course", courseSchema);
 
 export default COURSE;
