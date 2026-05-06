d4ef48c311ab0551728409ed8ec1a286b33e3d9d
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-05-06 21:02:20 +0530

    solve the cors error

--- Server/app.js
+++ Server/app.js
@@ -10,13 +10,9 @@ import miscellaneousRoute from './routes/miscellaneous.route.js'
 
 export const app = express();
 
-app.use(express.json());
 app.use(urlencoded({extended : true}));
-
-app.use(cors ({
-    origin : [process.env.FRONTEND_URL],
-    credentials : true
-}));
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 app.use(cookieParser());
 app.use(morgan('dev'));
