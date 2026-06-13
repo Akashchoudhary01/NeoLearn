f0ec4b639e92b74e0cbffb2077e13ca5fcc30905
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-06-13 23:17:45 +0530

    change the cors url

--- Server/app.js
+++ Server/app.js
@@ -12,7 +12,12 @@ export const app = express();
 
 app.use(urlencoded({extended : true}));
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(
+  cors({
+    origin: "https://neo-learn-c6bvtoqln-akash-choudharys-projects-65a64267.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(morgan('dev'));
